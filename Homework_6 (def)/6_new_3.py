5d45e31950c62a12b1135013ecac01f036280119
julia <julia0602k@gmail.com>
2023-01-14 23:34:31 +0300

    14.01 (15)

--- Homework_6 (def)/6_new_3.py
+++ Homework_6 (def)/6_new_3.py
@@ -3,4 +3,15 @@
 # пользователя, кто пригласил данного пользователя и может быть пустым, посчитать
 # для каждого пользователя, каждому пользователю добавить ключ referrals_count с
 # количеством человек, которых он пригласил
+users = [
+    {'id': 11111, 'name': 'Vasya', 'referrer_id': ''},
+    {'id': 21111, 'name': 'Masha', 'referrer_id': 11111},
+    {'id': 31111, 'name': 'Kate', 'referrer_id': ''},
+    {'id': 41111, 'name': 'Lena', 'referrer_id': 31111},
+    {'id': 51111, 'name': 'Kostya', 'referrer_id': 41111},
+    {'id': 61111, 'name': 'Vera', 'referrer_id': 41111},
+    {'id': 71111, 'name': 'Sanya', 'referrer_id': 51111},
+    {'id': 81111, 'name': 'Lera', 'referrer_id': 5111},
+    {'id': 91111, 'name': 'Vasilisa', 'referrer_id': 71111}
+]
 
