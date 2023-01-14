ef90cc81088e770db82a898b53d3e84e8e08d406
julia <julia0602k@gmail.com>
2023-01-14 15:17:09 +0300

    14.01 (13)

--- Homework_6 (def)/6_7.py
+++ Homework_6 (def)/6_7.py
@@ -3,9 +3,7 @@
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 def sum_neighbours(list1):
     for i in range(len(list1)):
-        if i == 0:
-            sum1 = list1[1] + list1[len(list1)-1]
-        elif 1 <= i <= (len(list1)-2):
+        if 0 <= i <= (len(list1)-2):
             sum1 = list1[i-1] + list1[i+1]
         else:
             sum1 = list1[0] + list1[len(list1) - 2]
