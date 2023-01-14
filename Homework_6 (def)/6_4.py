cf81c1d2cee7e04c1a7e659f2136795839765a7b
julia <julia0602k@gmail.com>
2023-01-14 22:35:19 +0300

    14.01 (15)

--- Homework_6 (def)/6_4.py
+++ Homework_6 (def)/6_4.py
@@ -4,3 +4,10 @@ list1 = [12, True, 'hello', 111, 15.1, 'python']
 list1 = list(filter(lambda i: isinstance(i, str), list1))
 print(list1)
 
+# def func111(list1):
+#     for i in range(len(list1)-1, -1, -1):
+#         if not isinstance(list1[i], str):
+#             del list1[i]
+#     return list1
+# print(func111(list1))
+
