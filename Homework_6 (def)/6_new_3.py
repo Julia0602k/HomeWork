afa51a13b502b14607483ed5a77e9a9ff1538eb8
julia <julia0602k@gmail.com>
2023-01-14 23:46:34 +0300

    14.01 (23)

--- Homework_6 (def)/6_new_3.py
+++ Homework_6 (def)/6_new_3.py
@@ -1,7 +1,7 @@
 # 3. На вход функции поступает список словарей users, каждый словарь содержит ключи:
 # id, name, referrer_id, id у каждого пользователя уникальный, referrer_id содержит id
-# пользователя, кто пригласил данного пользователя и может быть пустым, посчитать
-# для каждого пользователя, каждому пользователю добавить ключ referrals_count с
+# пользователя, кто пригласил данного пользователя и может быть пустым,
+# посчитать для каждого пользователя, каждому пользователю добавить ключ referrals_count с
 # количеством человек, которых он пригласил
 users = [
     {'id': 11111, 'name': 'Vasya', 'referrer_id': ''},
@@ -11,7 +11,15 @@ users = [
     {'id': 51111, 'name': 'Kostya', 'referrer_id': 41111},
     {'id': 61111, 'name': 'Vera', 'referrer_id': 41111},
     {'id': 71111, 'name': 'Sanya', 'referrer_id': 51111},
-    {'id': 81111, 'name': 'Lera', 'referrer_id': 5111},
+    {'id': 81111, 'name': 'Lera', 'referrer_id': 51111},
     {'id': 91111, 'name': 'Vasilisa', 'referrer_id': 71111}
 ]
+def refer(users: list[dict]):
+    list_ref = []
+    for i in users:
+        list_ref.append(i.get('referrer_id'))
+    for i in users:
+        i.setdefault('referrals_count', list_ref.count(i.get('id')))
+    return users
 
+print(refer(users))
