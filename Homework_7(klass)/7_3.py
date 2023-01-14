b539f8fc016a439285ae745fbbf131a8f24b8056
julia <julia0602k@gmail.com>
2023-01-14 15:01:34 +0300

    14.01 (13)

--- Homework_7(klass)/7_3.py
+++ Homework_7(klass)/7_3.py
@@ -3,3 +3,19 @@
 # Написать метод average — возвращающий среднее арифметическое между всеми числами
 # Написать метод max_count — возвращающий число из списка, которое чаще встречается,
 # если таких чисел несколько, вывести среднее арифметическое среди таких чисел
+class Numbers():
+    def __init__(self, numbers: list[int]):
+        self.numbers = numbers
+
+    def average(self):
+        sum = 0
+        for i in self.numbers:
+            sum+=i
+        return sum/len(self.numbers)
+    def max_count(self):
+        pass
+
+    def __str__(self):
+        return f'Среднее арифметическое среди всех чисел: {self.average()} \n'
+list1 = Numbers([1,2,3,4,5])
+# print(list1.average())
