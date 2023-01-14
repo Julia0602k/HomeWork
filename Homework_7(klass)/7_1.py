3e1779e657ef6b348da8e7e0b7e2726ab71266f6
julia <julia0602k@gmail.com>
2023-01-14 14:21:34 +0300

    14.01 (13)

--- Homework_7(klass)/7_1.py
+++ Homework_7(klass)/7_1.py
@@ -1,5 +1,19 @@
 # 1. Написать класс Student
 # Конструктор класса принимает аргументы: first_name: str, group: int, marks: list[int]
 # Написать метод __str__ возвращающая форматированную строку с данными об студенте
-# Написать функцию (не метод) student_sort принимающая список студентов: students:
-# list[Student] и возвращающая список студентов отсортированный по имени
+# Написать функцию (не метод) student_sort принимающая список студентов: students: list[Student] и возвращающая список
+# студентов отсортированный по имени
+
+class Student():
+    def __init__(self, first_name: str, group: int, marks: list[int]):
+        self.first_name = first_name
+        self.group = group
+        self.marks = marks
+    def __str__(self):
+        return f'Cтудент {self.first_name} из группы N {self.group} имеет отметки {self.marks}'
+# def student_sort(students: list[Student]):
+#     return students.sort()
+students = [Student('Василий', 231, [5, 8, 7]), Student('Мария', 156, [1, 8, 3, 7]), Student('Евгений', 568, [9, 8, 9, 9, 9])]
+vasya = Student('Василий', 231, [5, 8, 7])
+print(vasya)
+print(students)
--- Homework_7(klass)/7_2.py
+++ Homework_7(klass)/7_2.py
@@ -4,3 +4,23 @@
 # Написать метод perimeter возвращающий периметр получившегося прямоугольника
 # Написать метод square возвращающий площадь получившегося прямоугольникаы
 # *учесть, что координаты на плоскости могут быть отрицательными
+
+class Rectangle():
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    def perimeter(self):
+        perimeter_rectangle = abs(self.x2 - self.x1)*2 + abs(self.y2 - self.y1)*2
+        return perimeter_rectangle
+    def square(self):
+        square_rectangle = abs(self.x2 - self.x1)*abs(self.y2 - self.y1)
+        return square_rectangle
+    def print(self):
+        print(f'Периметр прямоугольника равен {self.perimeter()} см, площадь прямоугольника равен {self.square()} см2')
+ex_1 = Rectangle(1, 1, 3, 5)
+ex_2 = Rectangle(1, -1, -3, 8)
+ex_1.print()
+ex_2.print()
+
