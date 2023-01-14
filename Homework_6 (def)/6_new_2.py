e8282a1254e9e4123d25d18ac5b7d01a46e86679
julia <julia0602k@gmail.com>
2023-01-14 23:27:31 +0300

    14.01 (15)

--- Homework_6 (def)/6_new_2.py
+++ Homework_6 (def)/6_new_2.py
@@ -1,4 +1,31 @@
 # 2. На вход функции поступает список словарей humans и gender (m/w), каждый словарь
 # содержит ключи: gender(пол), height(рост) людей, вернуть средний рост людей
 # указанного пола
+humans = [
+    {'gender': 'w', 'height': 168},
+    {'gender': 'm', 'height': 178},
+    {'gender': 'w', 'height': 161},
+    {'gender': 'm', 'height': 185},
+    {'gender': 'w', 'height': 175},
+    {'gender': 'm', 'height': 181},
+    {'gender': 'w', 'height': 167},
+    {'gender': 'm', 'height': 193}
+    ]
+def average_height(humans: list[dict[str, str]]):
+    height_m = 0
+    count_m = 0
+    height_w = 0
+    count_w = 0
+    for i in humans:
+        if 'm' in i.values():
+            height_m += i.get('height')
+            count_m += 1
+        else:
+            height_w += i.get('height')
+            count_w += 1
+    return f'Средний рост мужчин {height_m/count_m} см, Средний рост женщин {height_w/count_w} см'
+print(average_height(humans))
+
+
+
 
