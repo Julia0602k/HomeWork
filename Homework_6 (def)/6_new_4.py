dd9302fa1f8474ed9e19f858931a22503c6a9c59
julia <julia0602k@gmail.com>
2023-01-15 00:35:01 +0300

    14.01 (23)

--- Homework_6 (def)/6_new_4.py
+++ Homework_6 (def)/6_new_4.py
@@ -6,13 +6,25 @@
 # указаны названия товаров, их количество, общая стоимость и общая стоимость с
 # учетом скидок
 products = [
-    {'id': 112345, 'title': 'Телевизор', 'price': 500, 'discount': 1},
-    {'id': 212345, 'title': 'Холодильник', 'price': 700, 'discount': 2},
+    {'id': 112345, 'title': 'Телевизор', 'price': 1000, 'discount': 1},
+    {'id': 212345, 'title': 'Холодильник', 'price': 1200, 'discount': 2},
     {'id': 312345, 'title': 'Принтер', 'price': 200, 'discount': 3},
-    {'id': 412345, 'title': 'Ноутбук', 'price': 1500, 'discount': 4},
+    {'id': 412345, 'title': 'Ноутбук', 'price': 3300, 'discount': 4},
     {'id': 512345, 'title': 'Смартфон', 'price': 300, 'discount': 5},
-    {'id': 612345, 'title': 'Кофемашина', 'price': 1000, 'discount': 6},
+    {'id': 612345, 'title': 'Кофемашина', 'price': 2000, 'discount': 6},
     ]
-def check(list1: list[tuple]):
-
-    return f'''Товар {'title'}: количество: {}шт, общая стоимость {'price'*}'''
+list_buy = [(212345, 1), (512345, 2), (612345, 1)]
+def check(list_buy: list[tuple[int, int]], products: list[dict]):
+    str1 = ''
+    sum_all = 0
+    sum_all_discount = 0
+    for i in list_buy:
+        for j in products:
+            if i[0] in j.values():
+                str1 += j.get('title') + ' - ' + str(i[1]) + ' шт.,'
+                sum_all += j.get('price') * i[1]
+                sum_all_discount += j.get('price') * i[1] * (100 - j.get('discount'))/100
+    return f'''Куплены товары: {str1}
+общая стоимость товаров: {sum_all} руб,
+общая стоимость товаров с учетом скидки: {sum_all_discount} руб'''
+print(check(list_buy, products))
