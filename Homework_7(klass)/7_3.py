bdedb91162907c56f9610a23ff9997b93936bf79
julia <julia0602k@gmail.com>
2023-01-18 00:43:44 +0300

    17.01(12)

--- Homework_7(klass)/7_3.py
+++ Homework_7(klass)/7_3.py
@@ -8,18 +8,20 @@ class Numbers:
         self.numbers = numbers
 
     def average(self):
-        return sum(self.numbers)/len(self.numbers)
+        return sum(self.numbers) / len(self.numbers)
 
     def max_count(self):
         from collections import Counter
         num_counter = Counter(self.numbers)
-        max_count_num = num_counter.most_common(n=1)[0][1]
+        max_count_num = num_counter.most_common(1)[0][1]
         numbers = list(filter(lambda x: self.numbers.count(x) == max_count_num, self.numbers))
-        return numbers
+        return sum(numbers) / len(numbers)
+
     def __str__(self):
         return f'''Среднее арифметическое среди всех чисел: {self.average()}, наиболее часто встречающиеся числа: {self.max_count()}'''
 
-list1 = Numbers([1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 3, 3, 3, 3])
+
+list1 = Numbers([1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 3, 3, 3, 3, 1, 1, 1])
 print(list1.average())
 print(list1.max_count())
 print(list1)
