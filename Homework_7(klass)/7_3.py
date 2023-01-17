3691c4e4391314b4b9c53b2dd0892286dd2b049e
julia <julia0602k@gmail.com>
2023-01-18 00:30:04 +0300

    17.01(12)

--- Homework_7(klass)/7_3.py
+++ Homework_7(klass)/7_3.py
@@ -3,17 +3,23 @@
 # Написать метод average — возвращающий среднее арифметическое между всеми числами
 # Написать метод max_count — возвращающий число из списка, которое чаще встречается,
 # если таких чисел несколько, вывести среднее арифметическое среди таких чисел
-class Numbers():
+class Numbers:
     def __init__(self, numbers: list[int]):
         self.numbers = numbers
 
     def average(self):
         return sum(self.numbers)/len(self.numbers)
 
+    def max_count(self):
+        from collections import Counter
+        num_counter = Counter(self.numbers)
+        max_count_num = num_counter.most_common(n=1)[0][1]
+        numbers = list(filter(lambda x: self.numbers.count(x) == max_count_num, self.numbers))
+        return numbers
     def __str__(self):
-        return f'''Среднее арифметическое среди всех чисел: {self.average()}'''
+        return f'''Среднее арифметическое среди всех чисел: {self.average()}, наиболее часто встречающиеся числа: {self.max_count()}'''
 
 list1 = Numbers([1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 3, 3, 3, 3])
 print(list1.average())
-# print(list1)
-# print(list1.max_count())
+print(list1.max_count())
+print(list1)
--- Homework_8(oop)/8_4.py
+++ Homework_8(oop)/8_4.py
@@ -6,3 +6,33 @@
 # 4.2 Добавить метод make_unpublished принимающий индекс категории и меняющий
 # значение ключа is_published на False, если такого индекса нет, вызвать исключение
 # IndexError
+class Category:
+    categories = {}
+    @classmethod
+    def add(cls, category: str):
+        return cls.categories.setdefault(category, False)
+
+    @classmethod
+    def get(cls, category: str):
+        return cls.categories.get(category)
+
+    @classmethod
+    def delete(cls, category: str) -> None:
+        if category in cls.categories:
+            del cls.categories[category]
+
+    @classmethod
+    def update(cls, category):
+        return cls.categories.setdefault(category, False)
+
+    @classmethod
+    def make_published(cls, category):
+        if category in cls.categories:
+            cls.categories[category] = True
+        raise IndexError
+
+    @classmethod
+    def make_unpublished(cls, category):
+        if category in cls.categories:
+            cls.categories[category] = False
+        raise IndexError
