f59b8e7455109edf373a194bed4a0e0c19a43684
julia <julia0602k@gmail.com>
2023-01-31 15:15:34 +0300

    31.01

--- Homework_12(sql_alchemy, alembic)/models.py
+++ Homework_12(sql_alchemy, alembic)/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, TIMESTAMP, INTEGER, SMALLINT, INT, VARCHAR, BOOLEAN, ForeignKey, DECIMAL, create_engine
+from sqlalchemy import Column, TIMESTAMP, INTEGER, SMALLINT, INT, \
+    VARCHAR, BOOLEAN, ForeignKey, DECIMAL, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from datetime import datetime
 
@@ -16,9 +17,9 @@ class Category(Base):
 class Product(Base):
     __tablename__ = 'products'
 
-    name = Column(VARCHAR(64), unique=True, nullable=False)
-    descr = Column(VARCHAR(1024), unique=True, nullable=False)
-    price = Column(DECIMAL(8, 2), unique=True, default=1.0)
+    name = Column(VARCHAR(64), nullable=False)
+    descr = Column(VARCHAR(1024), nullable=True)
+    price = Column(DECIMAL(8, 2),  nullable=False)
     is_published = Column(BOOLEAN, default=True)
     category_id = Column(INT, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
 
